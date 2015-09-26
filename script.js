c9e547c94e81d515bb68262f91cc94d5537a5342
zeird <l1aqus@gmail.com>
2015-09-26 19:55:38 +0600

    Add better canvas metrics calculation

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 (function() {
 	'use strict';
 
-	var scaleFactor,
-	    canvas = document.getElementById('canvas'),
+	var canvas = document.getElementById('canvas'),
 	    ctx = canvas.getContext('2d'),
-	    CANVAS_DEFAULT_WIDTH = 640,
-	    CANVAS_DEFAULT_HEIGHT = 480;
+	    canvasMetrics = {
+	    	DEFAULT_WIDTH: 640,
+	    	DEFAULT_HEIGHT: 480,
+	    	width: null,
+	    	height: null,
+	    	scaleFactor: null,
+	    	offsetX: null,
+	    	offsetY: null,
+	    };
 
 	(function initCanvasResizing() {
 		var debounceTmt = null;
 
 		function setCanvasSize() {
-			var wWidth = window.innerWidth,
-			    wHeight = window.innerHeight;
-			canvas.width = wWidth;
-			canvas.height = wHeight;
-			console.log('Canvas size is set to ' + wWidth + 'x' + wHeight);
+			canvasMetrics.width = window.innerWidth;
+			canvasMetrics.height = window.innerHeight;
 
-			scaleFactor = Math.min(wWidth/CANVAS_DEFAULT_WIDTH, wHeight/CANVAS_DEFAULT_HEIGHT);
-			console.log('Scale factor is set to ' + scaleFactor);
+			canvas.width = canvasMetrics.width;
+			canvas.height = canvasMetrics.height;
+			console.log('Canvas size is set to ' + canvasMetrics.width + 'x' + canvasMetrics.height);
+
+			canvasMetrics.scaleFactor = Math.min(canvasMetrics.width/canvasMetrics.DEFAULT_WIDTH,
+			                                     canvasMetrics.height/canvasMetrics.DEFAULT_HEIGHT);
+			console.log('Scale factor is set to ' + canvasMetrics.scaleFactor);
+
+			canvasMetrics.offsetX = Math.floor((canvasMetrics.width - canvasMetrics.DEFAULT_WIDTH*canvasMetrics.scaleFactor)/2);
+			canvasMetrics.offsetY = Math.floor((canvasMetrics.height - canvasMetrics.DEFAULT_HEIGHT*canvasMetrics.scaleFactor)/2);
+			console.log('Offsets: x=' + canvasMetrics.offsetX + ', y=' + canvasMetrics.offsetY);
 		}
 
 		window.onresize = function() {
